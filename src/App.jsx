8211eab9ad8f43870763b6dbc6bf00a2d46769bc
narteyr <richmond.k.tettey.28@dartmouth.edu>
2025-03-24 23:33:40 -0400

    created_frotend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import NavBar from "./functional/NavBar.jsx";
+import Message from "./functional/Message.jsx";
+import "./resources/styles/App.css";
+import SearchBar from "./functional/SearchBar.jsx";
 
+function App() {
+    const [selectedSongs, setSelectedSongs] = useState({});
+    const [options, setOptions] = useState({});
+    const [search, setSearch] = useState("");
 
-function App(){
-
-    return(
-        <></>
-    )
+    return (
+        <section>
+            <NavBar />
+            <Message />
+            <SearchBar/>
+        </section>
+    );
 }
 
 export default App;
